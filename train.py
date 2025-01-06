7b9ef119d73601c5e24a743e39e776e570d8cea0
ZZZXGBS <954632426@qq.com>
2025-01-06 09:09:07 +0800

    Update train.py

--- train.py
+++ train.py
@@ -2,5 +2,5 @@ from ultralytics import YOLO
 if __name__ == '__main__':
 	# model = YOLO(rf"yolov8n-cls.pt")
 	# model.train(cfg=rf"config.yaml")
-	model = YOLO(rf"yolov8n-cls-resnet18.yaml")
+	model = YOLO(rf"ultralytics/cfg/models/v8/yolov8-cls-resnet18.yaml")
 	model.train(cfg=rf"config2.yaml")
