e3f67c6c0046193144201534f751963f46f635e1
ZZZXGBS <954632426@qq.com>
2025-01-08 09:18:48 +0800

    1

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 from ultralytics import YOLO
+
 if __name__ == '__main__':
-	# model = YOLO(rf"custom_data/yolov8n-cls.pt")
-	# model.train(cfg=rf"custom_data/config.yaml")
-	model = YOLO(rf"resnet18.yaml")
-	model.train(cfg=rf"custom_data/config2.yaml")
+    # model = YOLO(rf"custom_data/yolov8n-cls.pt")
+    # model.train(cfg=rf"custom_data/config.yaml")
+    model = YOLO(rf"resnet18.yaml")
+    model.train(cfg=rf"custom_data/config2.yaml")
--- train_det.py
+++ train_det.py
@@ -1,4 +1,5 @@
 from ultralytics import YOLO
+
 if __name__ == '__main__':
-	model = YOLO(rf"custom_data/config_det.yaml").load(rf"custom_data/yolov8n.pt")
-	model.train(cfg=rf"config.yaml")
+    model = YOLO(rf"yolov8n.yaml").load("custom_data/yolov8n.pt")
+    model.train(cfg=rf"custom_data/config_det.yaml")
