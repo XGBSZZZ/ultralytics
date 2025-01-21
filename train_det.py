bbddfe80fe83c260165881d7708442b682efd9e6
ZZZXGBS <954632426@qq.com>
2025-01-21 15:32:15 +0800

    Update train_det.py

--- train_det.py
+++ train_det.py
@@ -1,5 +1,5 @@
 from ultralytics import YOLO
 
 if __name__ == '__main__':
-    model = YOLO(rf"yolov8n.yaml").load("custom_data/yolov8n.pt")
-    model.train(cfg=rf"custom_data/config_det.yaml")
+    model = YOLO(rf"yolov8n.yaml").load("zzz_data/yolov8n.pt")
+    model.train(cfg=rf"zzz_data/config_det.yaml")
