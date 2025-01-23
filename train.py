2a3bca6aa5183af66634f0bc0d14df326324aa42
ZZZXGBS <954632426@qq.com>
2025-01-23 13:47:01 +0800

    1312312

--- train.py
+++ train.py
@@ -1,7 +1,8 @@
-from ultralytics import YOLO
+from ultralytics import YOLO, step3_pruning
 
 if __name__ == '__main__':
-    # model = YOLO(rf"custom_data/yolov8n-cls.pt")
-    # model.train(cfg=rf"custom_data/config.yaml")
-    model = YOLO(rf"resnet18.yaml")
-    model.train(cfg=rf"custom_data/config2.yaml")
+    model = YOLO("yolov8n-cls.yaml").load(rf"zzz_data/yolov8n-cls.pt")
+    model.train(cfg=rf"zzz_data/config.yaml")
+    # model = YOLO(rf"resnet18.yaml")
+    # model.train(cfg=rf"zzz_data/config2.yaml")
+    # step3_pruning(0.05)
