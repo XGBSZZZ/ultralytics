6e4d844e566b290295a77b2559bea8c27dd0ad26
ZZZXGBS <954632426@qq.com>
2025-02-27 09:00:20 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,8 +1,8 @@
 from ultralytics import YOLO, step3_pruning
 
 if __name__ == '__main__':
-    # model = YOLO("yolov8n-cls.yaml").load(rf"zzz_data/yolov8n-cls.pt")
-    # model.train(cfg=rf"zzz_data/config.yaml")
+    model = YOLO("yolov8n-cls.yaml").load(rf"zzz_data/yolov8n-cls.pt")
+    model.train(cfg=rf"zzz_data/config.yaml")
     # model = YOLO(rf"resnet18.yaml")
     # model.train(cfg=rf"zzz_data/config2.yaml")
-    step3_pruning(0.25)
+    # step3_pruning(0.25)
