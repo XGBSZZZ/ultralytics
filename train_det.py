75f0d2e828f05c3cfca0af29861c956a0c18d9f2
ZZZXGBS <954632426@qq.com>
2026-06-25 09:24:39 +0800

    0

--- train_det.py
+++ train_det.py
@@ -2,4 +2,7 @@ from ultralytics import YOLO
 
 if __name__ == '__main__':
     model = YOLO("yolov8n.yaml").load(rf"zzz_data/yolov8n.pt")
-    model.train(cfg=rf"zzz_data/config_det.yaml", zzzhide=0.5, zzzrotate=1.0, fliproi=1.0, fliproi_names=["SCREW", "T_MOS_HS", "R_MOS_HS", "M2_80", "M2_HS", "PCH_HS", "DDR", "BATTERY", "RM", "SN"])
+    model.train(cfg=rf"zzz_data/config_det.yaml",
+                zzzhide=0.5,
+                zzzrotate=1.0,
+                fliproi=1.0, fliproi_names=["SCREW", "T_MOS_HS", "R_MOS_HS", "M2_80", "M2_HS", "PCH_HS", "DDR", "BATTERY", "RM", "SN"])
